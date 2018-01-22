6d3315ba34acef1b47667a8785feea37d6afdcf3
Federico Tomassetti <federico@tomassetti.me>
2018-01-22 21:14:25 +0100

    refactor FailedPredicateException

--- antlr-kotlin-runtime-common/src/main/kotlin/org/antlr/v4/kotlinruntime/FailedPredicateException.kt
+++ antlr-kotlin-runtime-common/src/main/kotlin/org/antlr/v4/kotlinruntime/FailedPredicateException.kt
@@ -8,38 +8,35 @@ package org.antlr.v4.kotlinruntime
 import org.antlr.v4.kotlinruntime.atn.AbstractPredicateTransition
 import org.antlr.v4.kotlinruntime.atn.transitions.PredicateTransition
 
-
 private fun formatMessage(predicate: String?, message: String?): String {
     return message ?: "failed predicate: {$predicate}?"
 
 }
 
-/** A semantic predicate failed during validation.  Validation of predicates
+/**
+ * A semantic predicate failed during validation.  Validation of predicates
  * occurs when normally parsing the alternative just like matching a token.
  * Disambiguating predicate evaluation occurs when we test a predicate during
  * prediction.
  */
-class FailedPredicateException constructor(recognizer: Parser,
-                                                         val predicate: String? = null,
-                                                         message: String? = null) : RecognitionException(recognizer, recognizer.readInputStream()!!, recognizer.context!!, formatMessage(predicate, message)) {
+class FailedPredicateException constructor(recognizer: Parser, val predicate: String? = null, message: String? = null)
+    : RecognitionException(recognizer, recognizer.readInputStream()!!, recognizer.context!!,
+        formatMessage(predicate, message)) {
     var ruleIndex: Int = -1
     var predIndex: Int = -1
 
     init {
-        val s = recognizer.interpreter!!.atn.states.get(recognizer.state)
+        val s = recognizer.interpreter!!.atn.states[recognizer.state]
 
         val trans = s!!.transition(0) as AbstractPredicateTransition
         if (trans is PredicateTransition) {
-            this.ruleIndex = (trans as PredicateTransition).ruleIndex
-            this.predIndex = (trans as PredicateTransition).predIndex
+            this.ruleIndex = trans.ruleIndex
+            this.predIndex = trans.predIndex
         } else {
             this.ruleIndex = 0
             this.predIndex = 0
         }
-        TODO()
-        //this.offendingToken = recognizer.currentToken
+        this.offendingToken = recognizer.currentToken
     }
-
-
 
 }
